c5fc1e905df136895b24ba0091c524629c503222
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2026-07-10 22:41:07 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.31%) - LeetHub - Time: O(, Space: O(?)

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if (n%2==1) {
+            return 2*n;
+        }
+        else return n;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
