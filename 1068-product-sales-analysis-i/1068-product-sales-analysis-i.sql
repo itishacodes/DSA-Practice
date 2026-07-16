6a8ef1194fe329e815738803439b9231a117aeb6
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2026-07-16 11:27:23 +0530

    Time: 1259 ms (59.71%), Space: 0B (100%) - LeetHub

---
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+SELECT product_name, year, price
+FROM Sales s, Product p
+WHERE s.product_id = p.product_id
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
