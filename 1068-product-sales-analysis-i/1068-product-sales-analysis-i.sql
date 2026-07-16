9d3cfacb8472bf03fb4a024a89144d233e7fb4a7
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2026-07-16 11:30:13 +0530

    Time: 1381 ms (31.82%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 
 SELECT product_name, year, price
-FROM Sales s, Product p
-WHERE s.product_id = p.product_id
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
 
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
