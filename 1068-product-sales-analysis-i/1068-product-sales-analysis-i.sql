74fb07feacc143fd3e21c0b8d991099b16c96536
Itisha Maheshwari <itishamaheshwari2005@gmail.com>
2026-07-16 11:32:41 +0530

    Time: 1381 ms (31.82%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -5,6 +5,9 @@ FROM Sales s
 LEFT JOIN Product p
 ON s.product_id = p.product_id
 
+# can also use USING keyword for last line.
+# USING(product_id)
+
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 -- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
